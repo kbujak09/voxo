cec4408e575a13712aa20ec476535d8db23c60ad
Kacper Bujak <kaccpper2@gmail.com>
2025-02-03 00:39:40 +0100

    Auth pages and components init

--- src/pages/Auth/LoginPage.tsx
+++ src/pages/Auth/LoginPage.tsx
@@ -3,6 +3,7 @@ import Logo from '../../components/Logo/index';
 import styles from './auth.module.scss';
 
 const Login = () => {
+  
   return (
     <div className={styles.container}>
       <Logo size={36}/>
